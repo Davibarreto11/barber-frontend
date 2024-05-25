9f77c5189bc20d7debc2ce5af508802e08f3d879
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-05-25 20:33:16 -0300

    hooks auth and toast

--- src/hooks/Toast.tsx
+++ src/hooks/Toast.tsx
@@ -1,60 +1,70 @@
-import { createContext, useCallback, useContext, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-import { ToastContainer } from '../components/ToastContainer'
+
+import { ToastContainer } from '../components/ToastContainer';
+
 
 export interface ToastMessage {
-  id: string
-  type: 'success' | 'error' | 'info'
-  title: string
-  description: string
+  id: string;
+  type: 'success' | 'error' | 'info';
+  title: string;
+  description: string;
 }
+
 
 interface ToastContextState {
-  addToast: (message: Omit<ToastMessage, 'id'>) => void
-  removeToast: (id: string) => void
+  addToast: (message: Omit<ToastMessage, 'id'>) => void;
+  removeToast: (id: string) => void;
 }
 
-interface ToastProviderState {
-  children: any
+
+interface ToastProviderProps {
+  children: React.ReactNode;
 }
 
-const ToastContext = createContext<ToastContextState>({} as ToastContextState)
 
-export const ToastProvider: React.FC<ToastProviderState> = ({ children }) => {
-  const [messages, setMessages] = useState<ToastMessage[]>([])
+const ToastContext = createContext<ToastContextState | undefined>(undefined);
+
+
+export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
 
   const addToast = useCallback(({ type, title, description }: Omit<ToastMessage, 'id'>) => {
-    const id = uuidv4()
+    const id = uuidv4();
+
 
     const toast = {
       id,
       type,
       title,
-      description
-    }
+      description,
+    };
 
-    setMessages([...messages, toast])
-  }, [messages])
+
+    setMessages((prevMessages) => [...prevMessages, toast]);
+  }, []);
+
 
   const removeToast = useCallback((id: string) => {
-    setMessages(state => state.filter(message => message.id !== id))
-  }, [])
+    setMessages((prevMessages) => prevMessages.filter((message) => message.id !== id));
+  }, []);
+
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
       <ToastContainer messages={messages} />
     </ToastContext.Provider>
-  )
-}
+  );
+};
+
 
 export const useToast = (): ToastContextState => {
-  const context = useContext(ToastContext)
-
-  if (!context) {
-    throw new Error('useToast must be used within a ToastProvider')
+  const context = useContext(ToastContext);
+  if (context === undefined) {
+    throw new Error('useToast must be used within a ToastProvider');
   }
-
-  return context
-}
+  return context;
+};
